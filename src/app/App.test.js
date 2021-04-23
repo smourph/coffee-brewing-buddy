08e01258c667cae65c1a89724d2b25dc2e78f596
Julien De Conti <julien.de.conti@gmail.com>
2021-04-23 13:26:51 +0200

    feat: add a Router

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,5 +1,11 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
+
+beforeEach(() => {
+  // Push the Router basename into history
+  window.history.pushState({}, 'Default page', '/coffee-brewing-buddy/');
+});
 
 test('should render App', () => {
   render(<App />);
@@ -12,10 +18,31 @@ test('should render App', () => {
   const main = screen.getByRole('main');
   expect(main).toBeInTheDocument();
   expect(main).toBeVisible();
-  expect(main).toHaveTextContent(/Web app for tracking coffee brewing/);
 
   const footer = screen.getByRole('contentinfo');
   expect(footer).toBeInTheDocument();
   expect(footer).toBeVisible();
   expect(footer).toHaveTextContent(/.../);
+});
+
+test('should land on home page by default', () => {
+  render(<App />);
+
+  expect(screen.getByRole('main')).toHaveTextContent(/Web app for tracking coffee brewing/);
+});
+
+test('should land on 404 page', () => {
+  window.history.pushState({}, '404 page', '/coffee-brewing-buddy/404');
+
+  render(<App />);
+
+  expect(screen.getByRole('main')).toHaveTextContent(/Page not found!/);
+});
+
+test('should redirect on 404 page when path is bad', () => {
+  window.history.pushState({}, 'Wrong path', '/coffee-brewing-buddy/some/wrong/path');
+
+  render(<App />);
+
+  expect(screen.getByRole('main')).toHaveTextContent(/Page not found!/);
 });
